a4b3f10145add1f81b4e60cdefd0718d9c85c1bf
hojeong <parkhj062@gmail.com>
2026-01-11 15:29:08 +0900

    [80. 로그인 처리하기 - 직접 만든 세션 적용]

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -2,29 +2,29 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
     private final MemberRepository memberRepository;
+    private final SessionManager sessionManager;
 
 //    @GetMapping("/")
     public String home() {
         return "home";
     }
 
-    @GetMapping
+    //@GetMapping
     public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
         if(memberId == null) {
             return "home";
@@ -39,5 +39,14 @@ public class HomeController {
         return "loginHome";
     }
 
+    @GetMapping
+    public String homeLogin2(HttpServletRequest request, Model model) {
+        Member member = (Member)sessionManager.getSession(request);
+        if(member == null) {
+            return "home";
+        }
 
+        model.addAttribute("member", member);
+        return "loginHome";
+    }
 }
