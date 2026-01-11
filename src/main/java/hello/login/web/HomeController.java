2f068304c024882f2a203c51f1a1aeba7eb2bc37
hojeong <parkhj062@gmail.com>
2026-01-11 18:24:43 +0900

    [82. 로그인 처리하기 - 서블릿 HTTP 세션2]
    - @SessionAttribute(name, required)
    - session url off

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -51,7 +52,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping
+    //@GetMapping
     public String homeLogin3(HttpServletRequest request, Model model) {
         HttpSession session = request.getSession(false);
         if(session == null) {
@@ -59,6 +60,16 @@ public class HomeController {
         }
 
         Member member = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+        if(member == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", member);
+        return "loginHome";
+    }
+
+    @GetMapping
+    public String homeLogin3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member member, Model model) {
         if(member == null) {
             return "home";
         }
